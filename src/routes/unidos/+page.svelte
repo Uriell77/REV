186abd35bfce5f7eebff13804a9786a472f3520c
Uriell77 <hermoso77@gmail.com>
2024-09-20 20:21:27 -0400

    unidos

--- src/routes/unidos/+page.svelte
+++ src/routes/unidos/+page.svelte
@@ -1,17 +1,28 @@
 <script>
+    import {onMount,afterUpdate } from "svelte";
     import { spring } from 'svelte/motion';
     import {blur,fade} from "svelte/transition";
+    import novia from "$lib/images/novia.webp";
+    import novio from "$lib/images/novio.webp";
+    import flyer from "$lib/images/flyer.jpg";
+    import anillos from "$lib/images/anillos.webp";
+    import Feliz from "$lib/images/sonidos/serfeliz.mp3";
 
     let texto = [
-        "Por tanto el hombre dejará a su padre y a su madre y se unirá a su mujer, y serán una sola carne <br> Génesis 2:24",
-        "Por lo demás, cada uno de vosotros ame también a su mujer como a sí mismo; y la mujer respete a su marido. <br> Efesios 5:33",
-        "Y sobre todas estas cosas vestíos de amor, que es el vínculo perfecto.<br>Colosenses 3:14",
-        "Y si alguno prevaleciere contra uno, dos le resistirán; y cordón de tres dobleces no se rompe pronto.<br>Eclesiastés 4:12",
-        "Les estoy dando un mandamiento nuevo: que se amen los unos a los otros. Ámense tal como yo los amé <br>Juan 13:34",
-        "Así que no son ya más dos, sino una sola carne; por tanto, lo que Dios juntó, no lo separe el hombre.<br>Mateo 19:6"
+        "Hoy Dios tiene una palabra para tu  matrimonio",
+        "Por tanto el hombre dejará a su padre y a su madre y se unirá a su mujer, y serán una sola carne <br> <h5>Génesis 2:24</h5>",
+        "Por lo demás, cada uno de vosotros ame también a su mujer como a sí mismo; y la mujer respete a su marido. <br> <h5>Efesios 5:33</h5>",
+        "Y sobre todas estas cosas vestíos de amor, que es el vínculo perfecto.<br><h5>Colosenses 3:14</h5>",
+        "Y si alguno prevaleciere contra uno, dos le resistirán; y cordón de tres dobleces no se rompe pronto.<br><h5>Eclesiastés 4:12</h5>",
+        "Les estoy dando un mandamiento nuevo: que se amen los unos a los otros. Ámense tal como yo los amé <br><h5>Juan 13:34</h5>",
+        "Así que no son ya más dos, sino una sola carne; por tanto, lo que Dios juntó, no lo separe el hombre.<br><h5>Mateo 19:6</h5>"
 
     ]
 
+    let sound;
+
+
+    let openform = false;
     let radio = spring(0)
     radio.stifness = 1;
     radio.damping = 1;
@@ -22,233 +33,224 @@
     radiosom.damping = 0.5;
     radiosom.presition = 0.003;
 
+    let passer = 0
     let screen;
     let scroller;
     let content = "plano1";
 
     $: flager = false;
+    $: flagger = false;
 
-    function flag(){
-        flager = true;
-    }
-
-    function dat(e){
-        //console.log(e.target.scrollTop);
-        scroller = e.target.scrollTop;
-        screen = e.target.scrollHeight;
-        //console.log(Math.trunc((scroller * 100)/screen))
-        if(Math.trunc((scroller * 100)/screen) == 0){
-            content = "plano1";
-            radio.set(0);
-        }
-        else if(Math.trunc((scroller * 100)/screen) == 16){
-            content = "plano2";
-            radio.set(6.2);
-        }
-        else if(Math.trunc((scroller * 100)/screen) == 33){
-            content = "plano3";
-            radio.set(12.4);
-        }
-        else if(Math.trunc((scroller * 100)/screen) == 50){
-            content = "plano4";
-            radio.set(18.6);
-        }
-        else if(Math.trunc((scroller * 100)/screen) == 66){
-            content = "plano5";
-            radio.set(24.8);
-        }
-        else if(Math.trunc((scroller * 100)/screen) == 83){
-            content = "plano6"
-            radio.set(31);
-            flag();
-        }
-
-        if($radio == 31){
-            radio = 31;
+    function pass(){
+        passer += 1;
+        if(passer == 6){
+            flager = true;
+            flagger = true;
         }
     }
 
 
+    let multiplo = 5.1;
+    afterUpdate(()=>{
+        if(passer == 1){
+        sound.play();
+        sound.volume = 0.2;
+        }
+    })
 </script>
 
+
+<svelte:head>
+    <meta property="og:type" content="website" />
+<meta property="og:title" content="Invitacion Congreso de Matrinomio" />
+<meta property="og:description" content="Te invitamos a nuestra iglesia para recibir palabra de Dios para tu matrimonio" />
+<meta property="og:image" content="{anillos}" />
+<meta property="og:image:width" content="828" />
+<meta property="og:image:height" content="450" />
+<meta property="og:url" content="https://escuela.it/cursos/curso-avanzado-python" />
+<meta property="og:site_name" content="Unidos" />
+<meta property="fb:app_id" content="928977633900253" />
+</svelte:head>
+
 <svelte:window bind:innerHeight={screen}/>
-<div class="fondo " bind:this={scroller} on:scroll={dat}>
+<div class="fondo p-0" bind:this={scroller} >
 
-    <div class="ter">
-        <div class="section mat has-text-centered p-0">
-            {#key content}
-
-                <div class="novia p-0 m-0" style="left:{$radio}%" transition:fade={{duration:900}}>
-                    <div class="sombranovia"  ></div>
-                    <img src="/novia.webp" alt="novia" class="nov"  />
+    {#key passer}
+        <section class="section ter" >
+            <img src="{novia}" alt="" class="novia" style:left="{passer*multiplo}%" transition:fade|global="{{duration:2000}}">
+            <img src="{novio}" alt="" class="novio" style:right="{passer*multiplo}%" transition:fade|global="{{duration:2000}}">
+            <div class="sombranovia" style:left="{passer*multiplo}%" transition:fade|global="{{duration:2000}}"></div>
+            <div class="sombranovio" style:right="{passer*multiplo}%" transition:fade|global="{{duration:2000}}"></div>
+        </section>
+    {/key}
+    <section class="section tar">
+        <div class="container sevillana-font has-text-weight-bold is-size-4">
+            {#key passer}
+                <div in:fade={{duration:2000, delay:2000}}>
+                    {@html texto[passer]}
                 </div>
-
-                <div class="novio p-0 m-0" style="right:{$radio}%" transition:fade={{duration:900}}>
-                    <div class="sombranovio"  ></div>
-                    <img src="/novio.webp" alt="novio" class="novo"/>
-                </div>
-
             {/key}
         </div>
 
-        <div class="section plano1 sevillana-font is-size-3">
-            <div class="container has-text-centered has-text-weight-bold">
-                {#key content}
-                    <div class="container {content == "plano1" ? "" : "is-hidden"}" transition:fade={{duration:900}}>
-                        {@html texto[0]}
-                    </div> 
-                {/key}
-            </div>
-        </div>
-        <div class="section plano2 sevillana-font is-size-3">
-            <div class="container has-text-centered has-text-weight-bold">
-                {#key content}
-                    <div class="container {content == "plano2" ? "": "is-hidden"}" transition:fade={{duration:900}}>
-                        {@html texto[1]}
-                    </div>
-                {/key}
-            </div>
-        </div>
-        <div class="section plano3 sevillana-font is-size-3">
-            <div class="container has-text-centered has-text-weight-bold">
-                {#key content}
-                    <div class="container {content == "plano3" ? "": "is-hidden"}" transition:fade={{duration:900}}>
 
-                        {@html texto[2]}
+        {#if flagger}
+            <div></div>
+        {:else}
+            {#key passer}
+            <button class="siguiente button is-rounded is-link is-light" on:click="{pass}" in:fade={{duration:2000, delay:5000}}>
+                Siguiente
+            </button>
+            {/key}
+        {/if}
 
-                    </div>
-                {/key}
-            </div>
-        </div>
-        <div class="section plano4 sevillana-font is-size-3">
-            <div class="container has-text-centered has-text-weight-bold">
-                {#key content}
-                    <div class="container {content == "plano4" ? "": "is-hidden"}" transition:fade={{duration:900}}>
 
-                        {@html texto[3]}
+    </section>
 
-                    </div>
-                {/key}
-            </div>
-        </div>
-        <div class="section plano5 sevillana-font is-size-3">
-            <div class="container has-text-centered has-text-weight-bold">
-                {#key content}
-                    <div class="container {content == "plano5" ? "": "is-hidden"}" transition:fade={{duration:900}}>
-
-                        {@html texto[4]}
-
-                    </div>
-                {/key}
-            </div>
-        </div>
-        <div class="section plano6 sevillana-font is-size-3">
-            <div class="container has-text-centered has-text-weight-bold">
-                {#key content}
-                    <div class="container {content == "plano6" ? "": "is-hidden"}" transition:fade={{duration:900}}>
-
-                        {@html texto[5]}
-
-                    </div>
-                {/key}
-            </div>
-        </div>
+    <audio src="{Feliz}" type="audio/mpeg" bind:this={sound} autoplay></audio>
 
     </div>
 
-</div>
+
 
 
     <!-- Modal -->
     {#key flager}
-    <div class="modal {flager ? "is-active":""}" in:fade={{duration:900, delay:8000}} out:fade={{duration:900}}>
-        <div class="modal-background"></div>
-        <div class="modal-content">
-            <!-- Any other Bulma elements you want -->
+        <div class="modal {flager ? "is-active":""}" in:fade={{duration:900, delay:8000}} out:fade={{duration:900}}>
+            <div class="modal-background p-0"></div>
+
+
+            <div class="modal-content p-5">
+
+            {#if openform}
+
+                <section class="section">
+
+                    <form action="" class="">
+
+                        <div class="fleld m-5">
+                                Nombre Completo del Esposo
+                            <p class="control">
+                                    <input class="input is-rounded" type="text">
+                            </p>
+                        </div>
+
+                        <div class="fleld m-5">
+                                Nombre Completo de la Esposa
+                            <p class="control">
+                                    <input class="input is-rounded" type="text">
+                            </p>
+                        </div>
+
+                        <div class="fleld m-5">
+                            Numero de Transferencia
+                            <p class="control">
+                                    <input class="input is-rounded" type="text">
+                            </p>
+                        </div>
+
+                        <section class="section has-text-centered">
+                        <button class="mt-4 button is-rounded is-link is-light">
+                            Reportar mi Asistencia
+                        </button>
+                        </section>
+                    </form>
+
+                </section>
+
+                {:else}
+                <figure class="image is-3by4">
+                    <img src="{flyer}" alt="im"/>
+                </figure>
+
+                <section class="section has-text-centered">
+                    <button class="button is-rounded is-link is-light" on:click={()=>{openform = true}}>
+                        Asistire
+                    </button>
+                </section>
+
+            {/if}
+            </div>
+
         </div>
-        <button class="modal-close is-large" aria-label="close" on:click="{()=>{flager = false}}"></button>
-    </div>
-{/key}
+    {/key}
 
 
 <style>
 
-
-    .nov{
-        position:relative;
-        z-index:50;
-        width:100%;
-        height:100%;
-        top:-10%;
-    }
-    .novo{
-        position:relative;
-        z-index:50;
-        width:83%;
-        height:108%;
-        top:-16%;
+    .modal-content{
+        scroll:none;
+        width:100% !important;
+        height:100% !important;
     }
 
-    .sombranovia,.sombranovio{
-        width:100px;
-        height:40px;
-        border-radius:50%;
-        background-color:black;
-        position:relative;
-        transform:rotatex(50deg);
-        filter:blur(20px);
+    .modal-background{
+        background: hsla(277, 79%, 84%, 1) !important;
+
+        background: linear-gradient(90deg, hsla(277, 79%, 84%, 1) 0%, hsla(204, 95%, 77%, 1) 100%) !important;
+
+        background: -moz-linear-gradient(90deg, hsla(277, 79%, 84%, 1) 0%, hsla(204, 95%, 77%, 1) 100%) !important;
+
+        background: -webkit-linear-gradient(90deg, hsla(277, 79%, 84%, 1) 0%, hsla(204, 95%, 77%, 1) 100%) !important;
+    }
+
+
+    .novia,.novio,.sombranovio,.sombranovia{
+        position: absolute;
+        z-index:20;
+    }
+
+    .novia{
+        height: 30%;
+        width: 30%;
+        top:40px;
+    }
+    .novio{
+        height: 31%;
+        width: 20%;
+        top:26px;
     }
 
     .sombranovia{
-        width:130px;
-        left:4%;
-        top:97%;
-        z-index: 30;
-        transition: all 3s;
+        top: 30%;
+        width: 30%;
+        height:10%;
+        background-color: black;
+        border-radius:50%;
+        transform:rotatex(50deg);
+        z-index:10;
+        filter:blur(40px);
     }
     .sombranovio{
-        right:-10%;
-        top:97%;
-        z-index: 20;
-        transition: all 3s;
-    }
-
-    .novia{
-        position:absolute;
-        top:13px;
-        width:25%;
-        height:80%;
-        z-index:20;
-    }
-    .novio{
-        position:absolute;
-        top:13px;
-        width:25%;
-        height:80%;
-        z-index:20;
-    }
-
-    .novio{
-        transition: all 3s;
-    }
-    .novia{
-        transition: all 3s;
-    }
-
-    .mat{
-        position:fixed;
-        top:0%;
-        background-color: rgba(255,255,255,.9);
-        width:100%;
-        height:40%;
+        top: 30%;
+        width: 30%;
+        height:10%;
+        background-color: black;
+        border-radius:50%;
+        transform:rotatex(50deg);
         z-index:10;
-        background: hsla(204, 95%, 77%, 1);
+        filter:blur(40px);
+    }
 
-        background: linear-gradient(90deg, hsla(204, 95%, 77%, 0) 29%, hsla(277, 79%, 84%, 1) 52%, hsla(204, 95%, 77%, 1) 100%);
 
-        background: -moz-linear-gradient(90deg, hsla(204, 95%, 77%, 0) 29%, hsla(277, 79%, 84%, 1) 52%, hsla(204, 95%, 77%, 1) 100%);
+    .siguiente{
+        position:absolute;
+        bottom:10%;
+        left:50%; 
+        transform: translate(-50%, -50%)
 
-        background: -webkit-linear-gradient(90deg, hsla(204, 95%, 77%, 0) 29%, hsla(277, 79%, 84%, 1) 52%, hsla(204, 95%, 77%, 1) 100%);
+    }
+
+    .ter{
+        height: 45% !important;
+        text-align:center;
+        width:100%;
+        margin:0 auto;
+    }
+    .tar{
+        height: 45% !important;
+        text-align:center;
+        width:100%;
+        margin:0 auto;
     }
 
 
@@ -266,18 +268,7 @@
         background: -webkit-linear-gradient(90deg, hsla(277, 79%, 84%, 1) 0%, hsla(204, 95%, 77%, 1) 100%);
         transition: all 3s;
     }
-    .plano1,.plano2,.plano3,.plano4,.plano5,.plano6{
-        height:100vh;
-        scroll-snap-align: start;
-        transition: all 3s;
-        
-    }
 
-    .container{
-        position:relative;
-        top:55%;
-
-    }
 
     @media screen and (min-width: 600px) {
         .ter {
@@ -286,9 +277,10 @@
             margin:0 auto;
         }
 
-        .mat{
-            position:absolute;
+        .tar {
+            text-align:center;
             width:50%;
+            margin:0 auto;
         }
 
     }
